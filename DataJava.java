af44ac289311bf33cee94a7cb850fb810203086a
MayllanB <115962720+MayllanB@users.noreply.github.com>
2023-04-25 00:20:14 -0300

    Para mostrar a sua data atual com Java.

---
+++ DataJava.java
@@ -0,0 +1,13 @@
+package aprendendoDesafio;
+
+import java.util.Calendar;
+
+public class DataJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar c = Calendar.getInstance();
+		System.out.println("");
+	}
+
+}
